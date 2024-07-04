d8dddbdeec3a6adb1ac2c8ceaa406e2333456389
ujwal02 <68330247+ujwal02@users.noreply.github.com>
2024-07-04 16:23:00 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,12 +1,6 @@
-import java.util.Scanner;
 
 public class Main {
-
-    public static final Scanner keyboard = new Scanner(System.in);
-
     public static void main(String[] args) {
-        Generator generator = new Generator(keyboard);
-        generator.mainLoop();
-        keyboard.close();
+        System.out.println("Hi, I am Ujwal");
     }
 }
